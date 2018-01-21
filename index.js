b6bb6799236fe086859238d416acf45161d6bdae
Tara Ellicott <taraellicott@gmail.com>
2018-01-21 19:02:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,6 +75,9 @@ function endGame() {
 }
 
 function moveDodger(e) {
+    // if the key is the right arrow key, move the dodger to the right
+  // if the key is the left arrow key, move the dodger to the left
+  // use window.requestAnimationFrame(). move the element 4px)
   const code = e.which
 
   if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
